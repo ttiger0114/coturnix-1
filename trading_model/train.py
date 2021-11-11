27756740f69633557ac58a0ca2633d1fd7e6b34a
leeminseung <mkms3580@gmail.com>
2021-11-11 16:47:03 +0000

    tick log

--- trading_model/train.py
+++ trading_model/train.py
@@ -11,18 +11,24 @@ from utils.get_logger import get_logger
 from utils.earlystopper import EarlyStopper
 from utils.get_dataloader import get_dataloader
 
-def train_model(model, epochs, optimizer, start_time, device, early_stopper, logger):
-    # loss_fn = nn.MSELoss()
+def train_model(model, epochs, optimizer, start_time, device, early_stopper, logger, tick=False, tick_precision=100):
     loss_fn = nn.CrossEntropyLoss()
 
     train_losses = []
-    valid_losses = []
-
     train_accuracies = []
+    tick_train_losses = []
+    tick_train_accuracies = []
+    
+    valid_losses = []
     valid_accuracies = []
+    tick_valid_losses = []
+    tick_valid_accuracies = []
 
     best_val_loss = np.Inf
     train_dataloader, valid_dataloader = get_dataloader()
+
+    if tick:
+        train_tick_frequency = len(train_dataloader) // tick_precision
 
     for epoch in tqdm(range(epochs)):
         train_loss_sum = 0
@@ -33,7 +39,15 @@ def train_model(model, epochs, optimizer, start_time, device, early_stopper, log
         valid_sum_acc = 0
         valid_total_cnt = 0
 
-        for x, y in train_dataloader:
+        tick_train_loss_sum = 0
+        tick_train_sum_acc = 0
+        tick_train_total_cnt = 0
+
+        tick_valid_loss_sum = 0
+        tick_valid_sum_acc = 0
+        tick_valid_total_cnt = 0
+
+        for i, (x, y) in tqdm(enumerate(train_dataloader), total=len(train_dataloader)):
             out = model(x.to(device))
             y = y.long().to(device)
             loss = loss_fn(out, y)
@@ -46,13 +60,53 @@ def train_model(model, epochs, optimizer, start_time, device, early_stopper, log
             train_sum_acc += torch.sum(predict == y)
             train_total_cnt += len(y)
 
+            if tick:
+                tick_train_loss_sum += loss.item()
+                tick_train_sum_acc += torch.sum(predict == y)
+                tick_train_total_cnt += len(y)
+
+                if i % train_tick_frequency == 0 and i:
+                    tick_train_loss_mean = tick_train_loss_sum / train_tick_frequency
+                    tick_train_accuracy = tick_train_sum_acc / tick_train_total_cnt
+                    msg = '\t{} tick, Tick Train Mean Loss: {}, Tick Train Accuracy: {}'.format(i / train_tick_frequency, tick_train_loss_mean, tick_train_accuracy)
+                    logger.info(msg)
+
+                    tick_train_losses.append(tick_train_loss_mean)
+                    tick_train_accuracies.append(tick_train_accuracy)
+
+                    tick_train_loss_sum = 0
+                    tick_train_sum_acc = 0
+                    tick_train_total_cnt = 0
+
+                    with torch.no_grad():
+                        for x, y in valid_dataloader:
+                            out = model(x.to(device))
+                            y = y.long().to(device)
+                            loss = loss_fn(out, y)
+
+                            tick_valid_loss_sum += loss.item()
+                            predict = torch.argmax(out, dim=1)
+                            tick_valid_sum_acc += torch.sum(predict == y)
+                            tick_valid_total_cnt += len(y)
+
+                        tick_valid_loss_mean = tick_valid_loss_sum / len(valid_dataloader)
+                        tick_valid_accuracy = tick_valid_sum_acc / tick_valid_total_cnt
+
+                        tick_valid_losses.append(tick_valid_loss_mean)
+                        tick_valid_accuracies.append(tick_valid_accuracy)
+                            
+                        tick_valid_loss_sum = 0
+                        tick_valid_sum_acc = 0
+                        tick_valid_total_cnt = 0
+                    
+
         train_accuracy = train_sum_acc/train_total_cnt
         train_loss_mean = train_loss_sum/len(train_dataloader)
         msg = '{} Epoch, Train Mean Loss: {}, Train Accuracy: {}'.format(epoch, train_loss_mean, train_accuracy)
         logger.info(msg)
 
         with torch.no_grad():
-            for x, y in vallid_dataloader:
+            for x, y in valid_dataloader:
                 out = model(x.to(device))
                 y = y.long().to(device)
                 loss = loss_fn(out, y)
@@ -134,8 +188,7 @@ if __name__ == '__main__':
     system_logger.info('\n\n')
     system_logger.info(model)
 
-    mse_loss_function = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters())
 
     early_stopper = EarlyStopper(patience=20)
-    train_model(model=model, optimizer=optimizer, epochs=3, start_time=start_time, device=DEVICE, early_stopper=early_stopper, logger=system_logger)
+    train_model(model=model, optimizer=optimizer, epochs=3, start_time=start_time, device=DEVICE, early_stopper=early_stopper, logger=system_logger, tick=True, tick_precision=100)
